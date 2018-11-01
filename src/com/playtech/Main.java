e61ff76b594e583e04fd69d1aba0acac8ad3e338
Meelis Tolk <meelis.tolk@playtech.com>
2018-11-01 10:30:19 +0200

    meelis first

--- src/com/playtech/Main.java
+++ src/com/playtech/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("asdad");
         System.out.println("asdad123");
+        System.out.println("asdad333");
 
     }
 }
