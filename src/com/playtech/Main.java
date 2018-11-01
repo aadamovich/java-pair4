ac48196d07bff92b915d2d7ece0004e993fdf190
Siim Kallson <Siim.Kallson@playtech.com>
2018-11-01 10:41:37 +0200

    meelis first

--- src/com/playtech/Main.java
+++ src/com/playtech/Main.java
@@ -5,7 +5,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("asdad");
         System.out.println("asdad123");
-        System.out.println("asdad333");
-
     }
 }
